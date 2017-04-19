61363a94fa2860919399b4492d0089c11c725427
Uires Deivide <uiresdeivide1@gmail.com>
2017-04-18 23:53:00 -0300

    Estrutura condicional e verificação

--- programa.c
+++ programa.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
+#include <locale.h>
+#include <conio.h>
 
 
 
 int main(){
+	setlocale(LC_ALL, "Portuguese");
 
-    printf("************************************\n");
-	printf("Bem vindo ao meu jogo de adivinhacao\n");
-	printf("************************************\n");
+    printf("******************************************************************************\n");
+	printf("Bem vindo ao meu jogo de adivinhação-------------------------------------------\n");
+	printf("******************************************************************************\n");
 	// declarações 
-	int numeroSecreto = 10;	
+	int numeroSecreto = 6;	
 	int chute;
-	
-	
-	printf("Qual o seu chute: (0 a 10) \n");
-	
-	scanf("%d", &chute);
-	
+		
+	printf("Qual o seu chute? (0 a 10) \n");	
+	scanf("%d", &chute);	
 	printf("Seu chute foi: %d\n",chute);
+	
 	// Estrutura condicional para verificar o número inserido pelo usuário
 	if(numeroSecreto == chute){
-		printf("Acertou !");
-	}else{
-		printf("Errou !");
+		printf("Acertou ! Parabéns !\n");
+	}
+	else{
+		printf("Errou !\n");
+		if(chute < numeroSecreto){
+			printf("O seu chute é menor que o número secreto...\n");
+		}else if(chute > numeroSecreto){
+			printf("O seu chute é maior que o número secreto...\n");
+		}		
+		printf("Tente de novo... ");
+		printf("\nBoa sorte...");
 	}
 	
 	
 	
-	
-	
+	system("PAUSE");
+	return 0;
 }
