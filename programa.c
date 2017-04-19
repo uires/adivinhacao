543f91980e9de1c87c91970fbf644f912e9b593c
Uires Deivide <uiresdeivide1@gmail.com>
2017-04-18 23:13:03 -0300

    Avançando mais na biblioteca IO

--- programa.c
+++ programa.c
@@ -3,9 +3,29 @@
 
 
 int main(){
-	printf("Bem vindo ao meu jogo de adivinhação");
-}
 
-
+    printf("************************************\n");
+	printf("Bem vindo ao meu jogo de adivinhacao\n");
+	printf("************************************\n");
+	int numeroSecreto = 10;
 	
-
+	int chute;
+	
+	
+	printf("Qual o seu chute: (0 a 10) \n");
+	
+	scanf("%d", &chute);
+	
+	printf("Seu chute foi: %d\n",chute);
+	// Estrutura condicional para verificar o número inserido pelo usuário
+	if(numeroSecreto == chute){
+		printf("Acertou !");
+	}else{
+		printf("Errou !");
+	}
+	
+	
+	
+	
+	
+}
