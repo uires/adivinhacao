ee94d6d85ab6f49d865d1e9ca7a435f0ff269ac6
Uires Deivide <uiresdeivide1@gmail.com>
2017-04-19 14:15:57 -0300

    Adicionando biblioteca matemática

--- programa.c
+++ programa.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <locale.h>
 #include <conio.h>
+#include <math.h>
 
 
 
-int main(){
+int main(void){
 	setlocale(LC_ALL, "Portuguese");
 
-    printf("******************************************************************************\n");
-	printf("Bem vindo ao meu jogo de adivinhação-------------------------------------------\n");
-	printf("******************************************************************************\n");
+    printf("************************************\n");
+	printf("Bem vindo ao meu jogo de adivinhação-\n");
+	printf("*************************************\n");
 	// declarações 
 	
 	int numeroSecreto = 6;	
 	int chute;
+	// For para dar 3 chances ao jogador
 	for(int i= 1; i <= 3; i++){
 	
 		printf("Qual o seu chute? (0 a 10) \n");	
 		scanf("%d", &chute);	
-		printf("Seu chute foi: %d\n",chute);
+		
+		printf("\n\nSeu chute foi: %d\n",chute);
 		
 		// Estrutura condicional para verificar o número inserido pelo usuário
 		if(numeroSecreto == chute){
@@ -29,7 +32,7 @@ int main(){
 			if(chute < numeroSecreto){
 				printf("O seu chute é menor que o número secreto...\n");
 			}else if(chute > numeroSecreto){
-				printf("O seu chute é maior que o número secreto...\n");
+				printf("O seu chute é maior que o número secreto...\n\n");
 			}		
 			printf("Tente de novo... ");
 			printf("\nBoa sorte...");
