40d133be28af5e2a0afbae7e516dc83e5c08d3ae
Uires Deivide <uiresdeivide1@gmail.com>
2017-04-22 01:10:26 -0300

    pequenas alterações e melhora no uso do loop

--- programa.c
+++ programa.c
@@ -8,24 +8,32 @@
 int main(void){
 	setlocale(LC_ALL, "Portuguese");
 
-    printf("************************************\n");
-	printf("Bem vindo ao meu jogo de adivinhação-\n");
-	printf("*************************************\n");
-	// declarações 
+	printf("\n\n");
+	printf("          P  /_\  P                              \n");
+	printf("         /_\_|_|_/_\                             \n");
+	printf("     n_n | ||. .|| | n_n         Bem vindo ao    \n");
+	printf("     |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!\n");
+	printf("    |" "  |  |_|  |"  " |                        \n");
+	printf("    |_____| ' _ ' |_____|                        \n");
+	printf("          \__|_|__/                              \n");
+	printf("\n\n");
 	
+	// declarações 
+	#define NUMERO_DE_TENTATIVAS 3;
 	int numeroSecreto = 6;	
 	int chute;
 	// For para dar 3 chances ao jogador
-	for(int i= 1; i <= 3; i++){
-	
-		printf("Qual o seu chute? (0 a 10) \n");	
-		scanf("%d", &chute);	
+	for(int i= 1; i <= NUMERO_DE_TENTATIVAS; i++){
 		
+		printf("Tentativa: %d\n", i);
+		printf("Qual o seu chute? (0 a 10) \n");			
+		scanf("%d", &chute);			
 		printf("\n\nSeu chute foi: %d\n",chute);
 		
 		// Estrutura condicional para verificar o número inserido pelo usuário
 		if(numeroSecreto == chute){
 			printf("Acertou ! Parabéns !\n");
+			break;
 		}
 		else{
 			printf("Errou !\n");
@@ -33,9 +41,10 @@ int main(void){
 				printf("O seu chute é menor que o número secreto...\n");
 			}else if(chute > numeroSecreto){
 				printf("O seu chute é maior que o número secreto...\n\n");
-			}		
-			printf("Tente de novo... ");
-			printf("\nBoa sorte...");
+			}if(i <= 3){
+				printf("Tente de novo...");
+				printf("\nBoa sorte...\n");		
+			}	
 		}
 	}			
 		
